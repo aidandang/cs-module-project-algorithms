be6f6a958cadff7204ac3f47f371a48659cc0862
Aidan Dang <aidandang@gmail.com>
2020-10-08 17:29:55 -0700

    completed single_number, product_of_all_other_numbers, moving_zeroes, sliding_windows_max

--- product_of_all_other_numbers/product_of_all_other_numbers.py
+++ product_of_all_other_numbers/product_of_all_other_numbers.py
@@ -3,9 +3,31 @@ Input: a List of integers
 Returns: a List of integers
 '''
 def product_of_all_other_numbers(arr):
-    # Your code here
+    n = len(arr) 
 
-    pass
+    if(n == 1): 
+        print(0) 
+        return
+          
+    left = [0]*n  
+    right = [0]*n  
+  
+    prod = [0]*n  
+  
+    left[0] = 1
+  
+    right[n - 1] = 1
+   
+    for i in range(1, n):  
+        left[i] = arr[i - 1] * left[i - 1]  
+   
+    for j in range(n-2, -1, -1):  
+        right[j] = arr[j + 1] * right[j + 1]  
+   
+    for i in range(n):  
+        prod[i] = left[i] * right[i] 
+    
+    return prod
 
 
 if __name__ == '__main__':
